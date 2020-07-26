3d3970902d4cbdadce49dfc9b77985753d056d87
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-07-25 23:04:06 -0400

    u-boot: Update to 2020.07 + graphics patch

--- devices/pine64-pinebookpro/u-boot/default.nix
+++ devices/pine64-pinebookpro/u-boot/default.nix
@@ -6,6 +6,7 @@
 , fetchFromGitLab
 , fetchFromGitHub
 , externalFirst ? true
+, runCommandNoCC
 }:
 
 let
@@ -15,18 +16,27 @@ let
     url = "https://patchwork.ozlabs.org/patch/${id}/raw/";
   };
 
+  # The version number for our opinionated firmware.
+  firmwareVersion = "002";
+
+  logo = runCommandNoCC "pbp-logo" {} ''
+    mkdir -p $out
+    cp ${../artwork/nixos+pine-rle.bmp} $out/logo.bmp                         
+    (cd $out; gzip -k logo.bmp)                           
+  '';                                    
+
   atf = armTrustedFirmwareRK3399.overrideAttrs(oldAttrs: {
     src = fetchFromGitHub {
       owner = "ARM-software";
       repo = "arm-trusted-firmware";
-      rev = "38aac6d4059ed11d6c977c9081a9bf4364227b5a";
-      sha256 = "0s08zrw0s0dvrc7229dwk6rzasrj3mrb71q232aiznnv9n5aszkz";
+      rev = "9935047b2086faa3bf3ccf0b95a76510eb5a160b";
+      sha256 = "1a6pm0nbgm5r3a41nwlkrli90l2blcijb02li7h75xcri6rb7frk";
     };
-    version = "2019-01-16";
+    version = "2020-06-17";
   });
 in
 (buildUBoot {
-  defconfig = "pinebook_pro-rk3399_defconfig";
+  defconfig = "pinebook-pro-rk3399_defconfig";
   extraMeta.platforms = ["aarch64-linux"];
   BL31 = "${atf}/bl31.elf";
   filesToInstall = [
@@ -36,42 +46,76 @@ in
   ];
 
   extraPatches = [
-    (pw "1194523" "07l19km7vq4xrrc3llcwxwh6k1cx5lj5vmmzml1ji8abqphwfin6")
-    (pw "1194524" "071rval4r683d1wxh75nbf22qs554spq8rk0499z6zac0x8q1qvc")
-    (pw "1194525" "0biiwimjp25abxqazqbpxx2wh90zgy3k786h484x9wsdvnv4yjl6")
-    (pw "1203678" "0l3l88cc9xkxkraql82pfgpx6nqn4dj7cvfaagh5pzfwkxyw0n3p")
+    # Upstream upcoming patches
+    # -------------------------
+    #
+    # https://patchwork.ozlabs.org/project/uboot/list/?series=182073
+    #
+#    # RNG
+#    # https://patchwork.ozlabs.org/patch/1305440/
+#    (pw "1305440" "1w4vvj3la34rsdf5snlvjl9yxnxrybczjz8m73891x1r6lvr1agk")
+    # USB
+    # https://patchwork.ozlabs.org/patch/1305441/
+    (pw "1305441" "1my6vz2j7dp6k9qdyf4kzyfy2fgvj4bhxq0xnjkdvsasiz7rq2x9")
+    # SPI has been skipped as it seemed to cause issues.
 
-    # Patches from this fork:
-    # https://git.eno.space/pbp-uboot.git
-    ./0001-rk3399-pinebook-fix-sdcard-boot-from-emmc.patch
-    ./0003-rk3399-light-pinebook-power-and-standby-leds-during-.patch
-    ./0004-reduce-pinebook_pro-bootdelay-to-1.patch
-    ./0005-PBP-Add-regulator-needed-for-usb.patch
+    # Upcoming patches
+    # ----------------
+    #
+    # These are not yet available on Patchwork. They are of *beta* quality.
+    # http://people.hupstream.com/~rtp/pbp/20200706/patches/series
+    #
+    # I have been authorised to distribute.
+    #
+    ./0001-display-support.patch
 
-    # My own patch
-    ./0001-HACK-Add-changing-LEDs-signal-at-boot-on-pinebook-pr.patch
+    # Dhivael patchset
+    # ----------------
+    #
+    # Origin: https://git.eno.space/pbp-uboot.git/
+    # Forward ported to 2020.07
 
+    ./0001-rk3399-light-pinebook-power-and-standby-leds-during-.patch
+    ./0002-reduce-pinebook_pro-bootdelay-to-1.patch
+    ./0005-support-SPI-flash-boot.patch
+
+    # samueldr's patchset
+    # -------------------
+    ./0001-opinionated-boot.patch
   ] ++ lib.optionals (externalFirst) [
-    # Patches from this fork:
-    # https://git.eno.space/pbp-uboot.git
-    ./0002-rockchip-move-mmc1-before-mmc0-in-default-boot-order.patch
-    ./0006-rockchip-move-usb0-after-mmc1-in-default-boot-order.patch
+    # Origin: https://git.eno.space/pbp-uboot.git/
+    # Forward ported to 2020.07
+    ./0003-rockchip-move-mmc1-before-mmc0-in-default-boot-order.patch
+    ./0004-rockchip-move-usb0-after-mmc1-in-default-boot-order.patch
   ];
+      
+  extraConfig = ''                                                                
+    CONFIG_IDENT_STRING=" (samueldr-pbp) v${firmwareVersion}"
+  '';             
 })
 .overrideAttrs(oldAttrs: {
   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
     python
   ];
+
   postPatch = oldAttrs.postPatch + ''
     patchShebangs arch/arm/mach-rockchip/
   '';
 
-  #src = lib.cleanSource /Users/samuel/tmp/u-boot/u-boot;
+  postInstall = ''
+    tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl-dtb.bin:spl/u-boot-spl-dtb.bin spl.bin
+    cat <(dd if=spl.bin bs=512K conv=sync) u-boot.itb > $out/u-boot.spiflash.bin
+  '';
+
+  makeFlags = oldAttrs.makeFlags ++ [
+    "LOGO_BMP=${logo}/logo.bmp"
+  ];
+
   src = fetchFromGitLab {
     domain = "gitlab.denx.de";
     owner = "u-boot";
     repo = "u-boot";
-    sha256 = "1fb8135gq8md2gr9sng1q2s1wj74xhy7by16dafzp4263b6vbwyv";
-    rev = "3ff1ff3ff76c15efe0451309af084ee6c096c583";
+    sha256 = "11154cxycw81dnmxfl10n2mgyass18jhjpwygqp7w1vjk9hgi4lw";
+    rev = "v2020.07";
   };
 })
