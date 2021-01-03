4cc876f80b2b4f0693c87eeb5c9452180dda2965
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-01-02 21:59:00 -0500

    chuwi-hi10prohq64: Add missing(?) firmwares

--- devices/chuwi-hi10prohq64/firmware/default.nix
+++ devices/chuwi-hi10prohq64/firmware/default.nix
@@ -1,6 +1,7 @@
 { lib
 , runCommandNoCC
 , fetchFromGitHub
+, firmwareLinuxNonfree
 }:
 
 
@@ -20,4 +21,18 @@ runCommandNoCC "chuwi-hi10prohq64-firmware" {} ''
   (cd ${gsl-firmware}
   cp -v firmware/chuwi/hi10_pro-z8350/firmware.fw $out/lib/firmware/silead/mssl1680.fw
   )
+
+  echo "-> Copying from firmwareLinuxNonfree"
+  for firmware in \
+    intel/fw_sst_22a8.bin \
+    rtl_bt/rtl8723bs_config-OBDA8723.bin \
+    rtl_bt/rtl8723bs_fw.bin \
+    rtlwifi/rtl8723bs_ap_wowlan.bin \
+    rtlwifi/rtl8723bs_bt.bin \
+    rtlwifi/rtl8723bs_nic.bin \
+    rtlwifi/rtl8723bs_wowlan.bin \
+  ; do
+    mkdir -p "$(dirname $out/lib/firmware/$firmware)"
+    cp -vrf "${firmwareLinuxNonfree}/lib/firmware/$firmware" $out/lib/firmware/$firmware
+  done
 ''
