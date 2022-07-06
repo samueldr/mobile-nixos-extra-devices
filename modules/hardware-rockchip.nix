cd2046d87795eb32f4a4d5bb01901b8aba962ac8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-06 16:31:36 -0400

    hardware-rockchip: Drop now removed options

--- modules/hardware-rockchip.nix
+++ modules/hardware-rockchip.nix
@@ -2,14 +2,7 @@
 
 let
   inherit (lib) mkIf mkMerge mkOption types;
-  inherit (pkgs) imageBuilder;
   cfg = config.mobile.hardware.socs;
-  initialGapSize = 
-    # Start of the "magic" location bs=512 seek=16384
-    (512 * 16384) +
-    # Current u-boot size: 838K, so let's leave a *bunch* of room.
-    (imageBuilder.size.MiB 2)
-  ;
 in
 {
   options.mobile = {
@@ -24,10 +17,6 @@ in
     {
       mobile = mkIf cfg.rockchip-rk3399.enable {
         system.system = "aarch64-linux";
-        quirks.u-boot = {
-          soc.family = "rockchip";
-          inherit initialGapSize;
-        };
       };
     }
   ];
