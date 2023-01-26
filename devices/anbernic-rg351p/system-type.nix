cb9a237ae855bb658931046a925ebea705d7fcc5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-26 00:02:56 -0500

    anbernic-rg351p: Add device-specific system type
    That target device has a vendor U-Boot build on SPI flash. Let's use it.
    
    Though it does have its own boot flow. So while it's not ideal, this
    makes this system compatible with unmodified units.

---
+++ devices/anbernic-rg351p/system-type.nix
@@ -0,0 +1,126 @@
+{ config, pkgs, lib, ... }:
+
+let
+  enabled = config.mobile.system.type == "odroid-style";
+
+  inherit (config.mobile.outputs) recovery stage-0;
+  inherit (pkgs) imageBuilder;
+  inherit (lib) mkIf mkOption types;
+  deviceName = config.mobile.device.name;
+  kernel = stage-0.mobile.boot.stage-1.kernel.package;
+  kernel_file = "${kernel}/${if kernel ? file then kernel.file else pkgs.stdenv.hostPlatform.linux-kernel.target}";
+
+  # GPIO a15 is the vibrator motor.
+  bootini = pkgs.writeText "${deviceName}-boot.ini" ''
+    ODROIDGO2-UBOOT-CONFIG
+
+    setenv dtb_name       "rk3326-rg351p-linux.dtb"
+    setenv ramdisk_addr_r "0x01100000"
+    setenv fdt_addr_r     "0x01f00000"
+    setenv kernel_addr_r  "0x02008000"
+
+    setenv dtb_name "rk3326-rg351p-linux.dtb"
+
+    gpio toggle a15
+    load mmc 1:1 ''${kernel_addr_r}   /mobile-nixos/boot/kernel
+    load mmc 1:1 ''${fdt_addr_r}      /mobile-nixos/boot/dtbs/rockchip/''${dtb_name}
+    load mmc 1:1 ''${ramdisk_addr_r} /mobile-nixos/boot/stage-1
+    setenv ramdisk_size ''${filesize}
+    gpio toggle a15
+
+    booti ''${kernel_addr_r} ''${ramdisk_addr_r}:''${ramdisk_size} ''${fdt_addr_r};
+
+    sleep 0.5
+    gpio toggle a15
+    sleep 0.5
+    gpio toggle a15
+    sleep 0.5
+    gpio toggle a15
+    sleep 0.5
+    gpio toggle a15
+    sleep 0.5
+  '';
+
+  # TODO: use generatedFilesystems
+  boot-partition =
+    imageBuilder.fileSystem.makeFAT32 {
+      name = "MN-BOOT";
+      partitionLabel = "MN-BOOT";
+      partitionID = "ABADF00D";
+      partitionUUID = "CFB21B5C-A580-DE40-940F-B9644B4466E1";
+      # Let's give us a *bunch* of space to play around.
+      size = imageBuilder.size.MiB 128;
+      bootable = true;
+      populateCommands = ''
+        mkdir -vp mobile-nixos/{boot,recovery}
+        (
+        cd mobile-nixos/boot
+        cp -v ${stage-0.mobile.outputs.initrd} stage-1
+        cp -v ${kernel_file} kernel
+        mkdir -p dtbs/rockchip
+        cp -t dtbs/rockchip -v ${kernel}/dtbs/rockchip/rk3326*rg351*dtb
+        )
+        (
+        cd mobile-nixos/recovery
+        cp -v ${recovery.mobile.outputs.initrd} stage-1
+        cp -v ${kernel_file} kernel
+        mkdir -p dtbs/rockchip
+        cp -t dtbs/rockchip -v ${kernel}/dtbs/rockchip/rk3326*rg351*dtb
+        )
+        cp -v ${bootini} ./boot.ini
+      '';
+    }
+  ;
+
+  # XXX explore if GPT is possible
+  disk-image = imageBuilder.diskImage.makeMBR {
+    name = "mobile-nixos";
+    diskID = "01234567";
+
+    partitions = [
+      boot-partition
+      config.mobile.outputs.generatedFilesystems.rootfs
+    ];
+  };
+in
+{
+  options.mobile = {
+    outputs = {
+      odroid-style = {
+        bootini = mkOption {
+          type = types.package;
+          internal = true;
+          visible = false;
+        };
+        boot-partition = mkOption {
+          type = types.package;
+          description = ''
+            Boot partition for the system.
+          '';
+          visible = false;
+        };
+        disk-image = lib.mkOption {
+          type = types.package;
+          description = ''
+            Full Mobile NixOS disk image for a u-boot-based system.
+          '';
+          visible = false;
+        };
+      };
+    };
+  };
+
+  config = lib.mkMerge [
+    { mobile.system.types = [ "odroid-style" ]; }
+    (mkIf enabled {
+      mobile.outputs = {
+        default = config.mobile.outputs.odroid-style.disk-image;
+        odroid-style = {
+          inherit bootini;
+          inherit boot-partition;
+          disk-image = disk-image;
+        };
+      };
+    })
+  ];
+}
