444d3eeb6e104e6bb21440f950134ebba07f562e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-26 00:40:28 -0500

    default.nix: review

--- default.nix
+++ default.nix
@@ -1,16 +1,33 @@
 { mobile-nixos ? <mobile-nixos>
+, pkgs ? import (mobile-nixos + /pkgs.nix) {}
 , device ? null
+, configuration ? {}
 }:
 
-let
-  deviceConfig = ./. + "/devices/${device}/default.nix";
-in
-
-if device == null then
-  throw "A device must be given, try adding `--argstr device device-name`."
-else if !(builtins.pathExists deviceConfig) then
-  throw "The device '${device}' could not be found."
-else if !(builtins.tryEval (builtins.pathExists mobile-nixos)).value then
+if !(builtins.tryEval (builtins.pathExists mobile-nixos)).value then
   throw "Mobile NixOS needs to be provided either through NIX_PATH or as an argument."
 else
-import (mobile-nixos) { device = deviceConfig; }
+
+let deviceArg = device; in
+let
+  device =
+    if builtins.isPath deviceArg
+      then deviceArg
+    else if builtins.isString deviceArg
+      then ./. + "/devices/${deviceArg}/default.nix"
+    else
+      builtins.throw ''
+        The `device` argument must be a path or a string.
+
+        TIP: use `--argstr device device-name` for a device defined in the devices directory.
+      ''
+  ;
+in
+
+import (mobile-nixos + /lib/eval-with-configuration.nix) {
+  inherit device;
+  inherit pkgs;
+  configuration = [
+    configuration
+  ];
+}
