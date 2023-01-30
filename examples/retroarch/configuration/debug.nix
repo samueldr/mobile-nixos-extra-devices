572e6e26e4c317019690e77abcbfac8a6278b3c0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-29 20:22:45 -0500

    retroarch: Add some more debugging config

--- examples/retroarch/configuration/debug.nix
+++ examples/retroarch/configuration/debug.nix
@@ -19,5 +19,24 @@ in
     input-utils
   ];
 
+  # If possible, enable ADB!
   mobile.adbd.enable = lib.mkDefault enableADB;
+
+  # Makes it so nix path registration happens, which in turn makes the store proper.
+  # Making the store proper means that `nix-copy-closure` and such works as expected.
+  nix.enable = lib.mkForce true;
+
+  # Silent boot is problematic when debugging
+  mobile.beautification.silentBoot = lib.mkForce false;
+  mobile.beautification.splash = lib.mkForce false;
+
+  # Re-enable console things
+  console.enable = lib.mkForce true;
+
+  systemd.services.games-session = {
+    serviceConfig = {
+      # We want "quit retroarch" to drop to a console when debugging issues.
+      Restart = lib.mkForce "on-failure";
+    };
+  };
 }
