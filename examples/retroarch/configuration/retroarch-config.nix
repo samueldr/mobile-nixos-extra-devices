36a2a2e50f284f6bd1885c4ed38e72fb7f9ff7cd
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-31 18:07:01 -0500

    retroarch: Add some config

--- examples/retroarch/configuration/retroarch-config.nix
+++ examples/retroarch/configuration/retroarch-config.nix
@@ -18,6 +18,19 @@ let
       "${k} = ${builtins.toJSON keptConfig."${k}"}"
     ) (attrNames keptConfig)
   );
+
+  joypadAutoconfig = pkgs.callPackage (
+    { runCommand }:
+
+    runCommand "retroarch-joypad-autoconfig" {
+      src = ./retroarch/joypad_autoconfig;
+    } ''
+      cp -prf $src $out
+    ''
+  ) {};
+
+  retroarch_stateful = "~/.config/retroarch";
+  retroarch_misc = "/userdata/Data/misc/retroarch";
 in
 {
   options = {
@@ -55,6 +68,139 @@ in
       thepowdertoy
     ];
     wip.retroarch.config = {
+      config_save_on_exit = "true";
+      core_info_cache_enable = "false";
+      joypad_autoconfig_dir = "${joypadAutoconfig}";
+      screenshot_directory = "/userdata/Screenshots";
+      savefile_directory   = "/userdata/Saves";
+      savestate_directory  = "/userdata/Saves/states";
+
+      log_dir                    = "${retroarch_stateful}/logs";
+      content_favorites_path     = "${retroarch_stateful}/content_favorites.lpl";
+      content_history_path       = "${retroarch_stateful}/content_history.lpl";
+      content_image_history_path = "${retroarch_stateful}/content_image_history.lpl";
+      content_music_history_path = "${retroarch_stateful}/content_music_history.lpl";
+      content_video_history_path = "${retroarch_stateful}/content_video_history.lpl";
+      cursor_directory           = "${retroarch_stateful}/database/cursors";
+      content_database_path      = "${retroarch_stateful}/database/rdb";
+      input_remapping_directory  = "${retroarch_stateful}/config/remaps";
+      playlist_directory         = "${retroarch_stateful}/playlists";
+      recording_config_directory = "${retroarch_stateful}/records_config";
+      recording_output_directory = "${retroarch_stateful}/records";
+      rgui_config_directory      = "${retroarch_stateful}/config";
+      thumbnails_directory       = "${retroarch_stateful}/thumbnails";
+
+      # Probably not feasible in a useful manner.
+      # We probably want a base skeleton we can ship with the image instead.
+      # core_options_path = "???";
+      # global_core_options = "???";
+
+      audio_filter_dir       = "${retroarch_misc}/filters/audio";
+      cheat_database_path    = "${retroarch_misc}/cheats";
+      core_assets_directory  = "${retroarch_misc}/downloads";
+      overlay_directory      = "${retroarch_misc}/overlay";
+      system_directory       = "${retroarch_misc}/system";
+      video_filter_dir       = "${retroarch_misc}/filters/video";
+      video_layout_directory = "${retroarch_misc}/layouts";
+      video_shader_dir       = "${retroarch_misc}/shaders";
+
+      # allow any RetroPad to control the menu
+      all_users_control_menu = "true";
+
+      # RetroPad button combination to toggle menu
+      # 0: None
+      # 1: Down + Y + L1 + R1
+      # 2: L3 + R3
+      # 3: L1 + R1 + Start + Select
+      # 4: Start + Select
+      # 5: L3 + R1
+      # 6: L1 + R1
+      # 7: Hold Start (2 seconds)
+      # 8: Hold Select (2 seconds)
+      # 9: Down + Select
+      # 10: L2 + R2
+      # NOTE: we're mapping L1+R1+START+SELECT as a fallback.
+      #       the supported bind is select as hotkey, with start for menu.
+      input_menu_toggle_gamepad_combo = "3";
+
+      # RetroPad button combination to quit
+      # 0: None
+      # 1: Down + Y + L1 + R1
+      # 2: L3 + R3
+      # 3: L1 + R1 + Start + Select
+      # 4: Start + Select
+      # 5: L3 + R1
+      # 6: L1 + R1
+      # 7: Hold Start (2 seconds)
+      # 8: Hold Select (2 seconds)
+      # 9: Down + Select
+      # 10: L2 + R2
+      input_quit_gamepad_combo = "0";
+
+      fps_show = "false";
+      memory_show = "false";
+
+      settings_show_accessibility = "true";
+      settings_show_audio = "true";
+      settings_show_configuration = "true";
+      settings_show_core = "true";
+      settings_show_directory = "true";
+      settings_show_drivers = "true";
+      settings_show_file_browser = "true";
+      settings_show_frame_throttle = "true";
+      settings_show_input = "true";
+      settings_show_latency = "true";
+      settings_show_logging = "true";
+      settings_show_onscreen_display = "true";
+      settings_show_playlists = "true";
+      settings_show_power_management = "true";
+      settings_show_recording = "true";
+      settings_show_saving = "true";
+      settings_show_user = "true";
+      settings_show_user_interface = "true";
+      settings_show_video = "true";
+
+      settings_show_network = "false";
+      settings_show_achievements = "false";
+      settings_show_ai_service = "false";
+
+      content_show_add = "true";
+      content_show_explore = "true";
+      content_show_history = "true";
+      content_show_settings = "true";
+      content_show_add_entry = "2";
+      #content_show_contentless_cores = "0";
+
+      content_show_favorites = "false";
+      content_show_images = "false";
+      content_show_music = "false";
+      content_show_netplay = "false";
+      content_show_playlists = "false";
+      content_show_video = "false";
+      # content_show_settings_password = "";
+
+      menu_show_advanced_settings = "true";
+      menu_show_configurations = "false";
+      menu_show_core_updater = "false";
+      menu_show_dump_disc = "false";
+      menu_show_help = "false";
+      menu_show_information = "true";
+      menu_show_latency = "true";
+      menu_show_legacy_thumbnail_updater = "false";
+      menu_show_load_content = "true";
+      menu_show_load_content_animation = "true";
+      menu_show_load_core = "true";
+      menu_show_load_disc = "false";
+      menu_show_online_updater = "false";
+      menu_show_overlays = "false";
+      menu_show_quit_retroarch = "false";
+      menu_show_reboot = "true";
+      menu_show_rewind = "true";
+      menu_show_shutdown = "true";
+      menu_show_sublabels = "true";
+      menu_show_video_layout = "false";
+      # Restart is buggy on some platforms and undesirable UX-wise...
+      menu_show_restart_retroarch = "false";
     };
     wip.retroarch.wrapped = pkgs.callPackage (
       { symlinkJoin
