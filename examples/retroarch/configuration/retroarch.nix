f56fc37078e41e39e1c2714e8390c9fe662ab02f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-01-31 19:45:02 -0500

    retroarch: Fixup service with cage quirk

--- examples/retroarch/configuration/retroarch.nix
+++ examples/retroarch/configuration/retroarch.nix
@@ -3,96 +3,116 @@
 
 let
   inherit (lib)
-    optionalString
+    escapeShellArgs
+    mkOption
+    optionals
+    types
   ;
+  inherit (config.wip.retroarch) quirks;
 in
 {
-  environment.systemPackages = with pkgs; [
-    cage
-    config.wip.retroarch.wrapped
-  ];
-
-  hardware.opengl.enable = true;
-
-  # Let userspace use the power key as a "menu" key.
-  services.logind.extraConfig = ''
-    HandlePowerKey=ignore
-  '';
-
-  # Signal VT1 ownership
-  systemd.services."getty@tty1" = {
-    enable = false;
-  };
-
-  systemd.services.games-session = {
-    enable = true;
-    after = [
-      "systemd-user-sessions.service"
-      "plymouth-start.service"
-      "plymouth-quit.service"
-      "systemd-logind.service"
-      "getty@tty1.service"
-    ];
-    before = [ "graphical.target" ];
-    wants = [ "dbus.socket" "systemd-logind.service" "plymouth-quit.service"];
-    wantedBy = [ "graphical.target" ];
-    partOf = [ "graphical.target" ];
-    conflicts = [
-      # Ensure there's no login prompt on the screen used for steam.
-      "getty@tty1.service"
-      # Ensures we don't run at the same time as display manager
-      "display-manager.service"
-    ];
-    restartIfChanged = false;
-    unitConfig.ConditionPathExists = "/dev/tty1";
-    serviceConfig = {
-      User = 1000;
-      PAMName = "login";
-      WorkingDirectory = "~";
-      TTYPath = "/dev/tty1";
-      TTYReset = "yes";
-      TTYVHangup = "yes";
-      TTYVTDisallocate = "yes";
-      StandardInput = "tty-fail";
-      StandardOutput = "journal";
-      StandardError = "journal";
-      UtmpIdentifier = "tty1";
-      UtmpMode = "user";
-      Restart = "always";
+  options = {
+    wip.retroarch = {
+      quirks = {
+        cage = {
+          enable = mkOption {
+            type = types.bool;
+            default = false;
+          };
+          parameters = mkOption {
+            type = with types; listOf str;
+            default = false;
+          };
+        };
+      };
     };
-    script = ''
-      ${pkgs.cage}/bin/cage \
-        -s \
-        ${optionalString (config.mobile.device.name == "anbernic-rg351p") "-r"} \
-        ${config.wip.retroarch.wrapped}/bin/retroarch
-    '';
   };
+  config = {
+    hardware.opengl.enable = true;
 
-  # Ensures graphical target is ran.
-  systemd.defaultUnit = "graphical.target";
+    # Let userspace use the power key as a "menu" key.
+    services.logind.extraConfig = ''
+      HandlePowerKey=ignore
+    '';
 
-  nixpkgs.overlays = [(
-    final: super: {
-      retroarch = final.callPackage ./retroarch { };
-      retroarch-assets = super.retroarch-assets.overrideAttrs({ postInstall ? "", ... }: {
-        postInstall = ''
-          (
-          cd $out/share/retroarch/assets
-          PS4=" $ "
-          set -x
-          rm sounds/BGM.wav
-          rm -r wallpapers
-          rm -r Systematic FlatUX Automatic
-          rm -r branding pkg switch
-          rm -r devtools
-          rm -r ctr
-          rm -r scripts
-          rm -r cfg
-          cd xmb
-          rm -r systematic neoactive retroactive automatic retrosystem dot-art daite pixel
-          )
-        '';
-      });
-    }
-  )];
+    # Signal VT1 ownership
+    systemd.services."getty@tty1" = {
+      enable = false;
+    };
+
+    systemd.services.games-session = {
+      enable = true;
+      after = [
+        "systemd-user-sessions.service"
+        "plymouth-start.service"
+        "plymouth-quit.service"
+        "systemd-logind.service"
+        "getty@tty1.service"
+      ];
+      before = [ "graphical.target" ];
+      wants = [ "dbus.socket" "systemd-logind.service" "plymouth-quit.service"];
+      wantedBy = [ "graphical.target" ];
+      partOf = [ "graphical.target" ];
+      conflicts = [
+        # Ensure there's no login prompt on the screen used for steam.
+        "getty@tty1.service"
+        # Ensures we don't run at the same time as display manager
+        "display-manager.service"
+      ];
+      restartIfChanged = false;
+      unitConfig.ConditionPathExists = "/dev/tty1";
+      serviceConfig = {
+        User = 1000;
+        PAMName = "login";
+        WorkingDirectory = "~";
+        TTYPath = "/dev/tty1";
+        TTYReset = "yes";
+        TTYVHangup = "yes";
+        TTYVTDisallocate = "yes";
+        StandardInput = "tty-fail";
+        StandardOutput = "journal";
+        StandardError = "journal";
+        UtmpIdentifier = "tty1";
+        UtmpMode = "user";
+        Restart = "always";
+      };
+      script = escapeShellArgs ([
+          "exec"
+        ] ++ optionals quirks.cage.enable ([
+          "${pkgs.cage}/bin/cage"
+        ] ++ quirks.cage.parameters)
+        ++ [
+          "${config.wip.retroarch.wrapped}/bin/retroarch"
+        ]
+      );
+    };
+
+    # Ensures graphical target is ran.
+    systemd.defaultUnit = "graphical.target";
+
+    nixpkgs.overlays = [(
+      final: super: {
+        retroarch = final.callPackage ./retroarch { };
+        retroarch-assets = super.retroarch-assets.overrideAttrs({ postInstall ? "", ... }: {
+          postInstall = ''
+            (
+            cd $out/share/retroarch/assets
+            PS4=" $ "
+            set -x
+            rm sounds/BGM.wav
+            rm -r wallpapers
+            rm -r Systematic FlatUX Automatic
+            rm -r branding pkg switch
+            rm -r devtools
+            rm -r ctr
+            rm -r scripts
+            rm -r cfg
+            cd xmb
+            rm -r systematic neoactive retroactive automatic retrosystem dot-art daite pixel
+            )
+          '';
+        });
+      }
+    )];
+  };
 }
